2262e5cf405ff3ce5122bc12f2342f1d90ff4198
alexbryw <alexbryw@gmail.com>
2019-12-19 22:18:06 +0100

    Added public splash input and removed splash edge overflow.

--- sketch/TargetGameCanvas.ts
+++ sketch/TargetGameCanvas.ts
@@ -2,25 +2,33 @@ class TargetGameCanvas implements MovingObject {
     // Class attributes
     velX: number
     velY: number
-    splashList: Array<{posX:number, posY:number, color: string}>
-    targetCanvasPosX: number = 0;
-    targetCanvasDirection: boolean = false;
+    
+    private splashList: Array<{posX:number, posY:number, color: string, splashDiameter: number}>
+    private targetCanvasPosX: number = 0;
+    private targetCanvasPosY: number = 0;
+    private targetCanvasWidth: number = 0;
+    private targetCanvasHeight: number = 0;
+    private targetCanvasDirection: boolean = false;
+    private cutOutImage: p5.Image = new p5.Image();
 
     // Class constructor
     constructor (velX: number, velY: number) {
         this.velX = velX
         this.velY = velY
+        
         this.splashList = [] //or new Array()
+        this.setTargetCanvasSize();
     }
     
     // Class functions
     public draw(): void {
         // Insert draw logic here
         this.updatePosition();
-        this.drawBackground();
-        this.drawTargetBoard();
-        this.drawSplash();
-        //this.testMissile();
+        this.drawTargetBoard();     //empty target canvas drawn first
+        this.drawSplash();          //then splashes
+        this.cutOutTargetCanvas();  //canvas is cut out to remove splashes outside target canvas
+        this.drawBackground();      //background is drawn over target canvas
+        this.drawTargetCutOutOnBackground();    //the cutout is added on top of background.
     }
 
     public updatePos(): number {
@@ -28,51 +36,63 @@ class TargetGameCanvas implements MovingObject {
         return 1
     }
 
+    private setTargetCanvasSize(){
+        this.targetCanvasWidth = windowWidth/2;
+        this.targetCanvasHeight = windowHeight/2;
+        this.targetCanvasPosY = windowHeight/4;
+    }
+
     private drawBackground(){
-        let i:number = 0
+        let i:number = 0;
         let stepHeight:number = 10;
-        noStroke()
+        push();
+        noStroke();
         while(i< windowHeight){
             fill(i/9,i/5,i/2);
             rect(0,i,windowWidth,i+stepHeight);
             i += stepHeight;
         }
+        pop();
     }
 
     private drawTargetBoard(){
-        push()
-        translate(this.targetCanvasPosX,0)
-        fill(255)
-        rect(windowWidth/4,windowHeight/4,windowWidth/2,windowHeight/2)
+        push();
+        noStroke();
+        fill(255);
+        rect(this.targetCanvasPosX,this.targetCanvasPosY,this.targetCanvasWidth,this.targetCanvasHeight)
         pop();
     }
 
     private drawSplash(){
-        push()
-        translate(this.targetCanvasPosX,0)
-        fill('green');
-        circle(windowWidth/2,windowHeight/2,80);
-        
+        push();
         for(let splash of this.splashList){
+            noStroke();
             fill(splash.color);
-            circle(splash.posX, splash.posY,60);
+            circle(splash.posX + this.targetCanvasPosX, splash.posY,splash.splashDiameter);
         }
-        pop()
+        pop();
     }
 
-    //TODO is click inside target box and full side to side motion.
+    private cutOutTargetCanvas(){
+        this.cutOutImage = get(this.targetCanvasPosX,this.targetCanvasPosY,this.targetCanvasWidth,this.targetCanvasHeight);
+    }
+
+    private drawTargetCutOutOnBackground(){
+        push();
+        noStroke();
+        image(this.cutOutImage,this.targetCanvasPosX,this.targetCanvasPosY);
+        pop();
+    }
+
+    //test for splash input TODO remove when done.
     public mouseClicked(){
-        this.isMissileInsideTarget(mouseX,mouseY)
-        
-        if(this.splashList.length > 3){
-            this.splashList.shift();
-        }
-        //console.log(this.splashList);
-        //console.log(this.targetCanvasPosX)
+        this.addSplashToTargetCanvas(mouseX, mouseY, 'green', random(30,120));
     }
 
+    //Move target canvas left to right.
     private updatePosition(){
-        let moveBand = 500
+        let moveBand = windowWidth/2
+        let canvasSpeed = 1;
         if(this.targetCanvasPosX > moveBand){
             this.targetCanvasDirection = true;
         }
@@ -81,30 +101,35 @@ class TargetGameCanvas implements MovingObject {
         }
 
         if(this.targetCanvasPosX >= 0 && this.targetCanvasDirection == false){
-            this.targetCanvasPosX +=1;
+            this.targetCanvasPosX += canvasSpeed;
         }
         else if(this.targetCanvasDirection == true){
-            this.targetCanvasPosX -=1;
+            this.targetCanvasPosX -= canvasSpeed;
         }
     }
 
-    //grey blob inside box, blue outside target canvas
-    private isMissileInsideTarget(hitPosX:number, hitPosY:number){
-        console.log(hitPosX,hitPosY);
-        if(hitPosX > this.targetCanvasPosX + windowWidth/4 && hitPosX < this.targetCanvasPosX + windowWidth*0.75
-            && hitPosY > windowHeight/4 && hitPosY < windowHeight*0.75)
-            {
-            this.splashList.push({posX:mouseX-this.targetCanvasPosX,posY:mouseY,color:'grey'})
+    //add splash to splashList if it's inside target canvas area.
+    private isMissileInsideTarget(hitPosX:number, hitPosY:number, splashColor: string, splashDiameter: number){
+
+        if(hitPosX > this.targetCanvasPosX - splashDiameter/2 && hitPosX < this.targetCanvasPosX + this.targetCanvasWidth + splashDiameter/2
+            && hitPosY > this.targetCanvasPosY - splashDiameter/2 && hitPosY < this.targetCanvasPosY + this.targetCanvasHeight + splashDiameter/2)
+        {
+            this.splashList.push({posX:hitPosX - this.targetCanvasPosX,posY:hitPosY,color: splashColor,splashDiameter: splashDiameter})
         }
         else{
-            this.splashList.push({posX:mouseX-this.targetCanvasPosX,posY:mouseY,color:'blue'})
+            //this.splashList.push({posX:mouseX - this.targetCanvasPosX,posY:mouseY,color:'blue',splashDiameter: 40})
         }
+
+        //max nr of splashes in list
+        if(this.splashList.length > 200){
+            this.splashList.shift();
+        }
+
     }
 
-    /* TODO P5 play
-    private testMissile(){
-        rect(0,this.targetCanvasPosX,60,60)
-        let circle = createSprite(100,100)
-    }*/
+    // Call this to add splashes to canvas.
+    public addSplashToTargetCanvas(hitPosX: number, hitPosY: number, splashColor: string, splashDiameter: number){
+        this.isMissileInsideTarget(hitPosX, hitPosY, splashColor, splashDiameter);
+    }
 
 }
